1758ace7db9905a134f674d7bb2b8c143526277b
Leandro Lucarella <luca-frequenz@llucax.com>
2023-08-08 16:40:52 +0200

    Add missing comments to test functions
    Now tests are checked for documentation.
    
    Signed-off-by: Leandro Lucarella <luca-frequenz@llucax.com>

--- pytests/test_microgrid.py
+++ pytests/test_microgrid.py
@@ -4,12 +4,14 @@
 """Tests for the frequenz.api.microgrid package."""
 
 def test_package_import() -> None:
+    """Test if the microgrid package can be imported."""
     import frequenz.api.microgrid as microgrid
 
     assert microgrid is not None
 
 
 def test_module_import() -> None:
+    """Test if the microgrid_pb2 package can be imported."""
     import frequenz.api.microgrid.microgrid_pb2 as microgrid_pb2
 
     assert microgrid_pb2 is not None
