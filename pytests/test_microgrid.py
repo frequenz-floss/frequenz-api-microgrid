34030f81c45aeabec97548613eb934ae3f46efe0
Leandro Lucarella <luca-frequenz@llucax.com>
2023-08-08 16:42:28 +0200

    Use `from x import y` instead of `import x.y as y`
    Also there is a pylint check for this, as the later format is redundant.
    
    Signed-off-by: Leandro Lucarella <luca-frequenz@llucax.com>

--- pytests/test_microgrid.py
+++ pytests/test_microgrid.py
@@ -8,17 +8,16 @@
 
 def test_package_import() -> None:
     """Test if the microgrid package can be imported."""
-    import frequenz.api.microgrid as microgrid
+    from frequenz.api import microgrid
 
     assert microgrid is not None
 
 
 def test_module_import() -> None:
     """Test if the microgrid_pb2 package can be imported."""
-    import frequenz.api.microgrid.microgrid_pb2 as microgrid_pb2
+    from frequenz.api.microgrid import microgrid_pb2
 
     assert microgrid_pb2 is not None
-
-    import frequenz.api.microgrid.microgrid_pb2_grpc as microgrid_pb2_grpc
+    from frequenz.api.microgrid import microgrid_pb2_grpc
 
     assert microgrid_pb2_grpc is not None
