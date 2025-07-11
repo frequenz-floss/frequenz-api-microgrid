26246b5e9eb5e410ae240ed678c0d95cec303ac5
Tiyash Basu <tiyash.basu@frequenz.com>
2025-07-11 10:29:41 +0200

    Recreate the `v1` directory and package with contents from v0.17.2
    Signed-off-by: Tiyash Basu <tiyash.basu@frequenz.com>

---
+++ pytests/test_microgrid_v1.py
@@ -0,0 +1,25 @@
+# License: MIT
+# Copyright © 2022 Frequenz Energy-as-a-Service GmbH
+
+"""Tests for the frequenz.api.microgrid package."""
+
+
+def test_package_import() -> None:
+    """Test that the package can be imported."""
+    # pylint: disable=import-outside-toplevel
+    from frequenz.api.microgrid import v1
+
+    assert v1 is not None
+
+
+def test_package_import_microgrid() -> None:
+    """Test that the package can be imported."""
+    # pylint: disable=import-outside-toplevel
+    from frequenz.api.microgrid.v1 import microgrid_pb2
+
+    assert microgrid_pb2 is not None
+
+    # pylint: disable=import-outside-toplevel
+    from frequenz.api.microgrid.v1 import microgrid_pb2_grpc
+
+    assert microgrid_pb2_grpc is not None
